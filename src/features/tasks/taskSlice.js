a0af0ebfee297aa5486258b2ca82eaaa12df6808
AlbertLnz <albert2000_lanza@hotmail.com>
2023-09-26 12:49:38 +0200

    Example taskReducer w/ initialState

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -1,8 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = [
+  {
+    id: "1",
+    title: "Task 1",
+    description: "Task 1 description",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    description: "Task 2 description",
+    completed: false,
+  },
+];
+
 const taskSlice = createSlice({
   name: "tasks",
-  initialState: [],
+  initialState, // === initialState: initialState
   reducers: {},
 });
 
